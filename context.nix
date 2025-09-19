6cef548bfbe38350044f53acbd59dcbd530c4d90
Hugo Herter <git@hugoherter.com>
2025-09-19 20:11:39 +0200

    move: past meetups to Nix context

--- context.nix
+++ context.nix
@@ -1,3 +1,66 @@
 {
   matrix_room = "#brunix:matrix.org";
+
+  past_meetups = [
+    {
+      number = 10;
+      venue_name = "HSBXL";
+      venue_url = "https://hsbxl.be/";
+      date = "March 28, 2025";
+    }
+    {
+      number = 9;
+      venue_name = "HSBXL";
+      venue_url = "https://hsbxl.be/";
+      date = "January 17th, 2024";
+    }
+    {
+      number = 8;
+      venue_name = "HSBXL";
+      venue_url = "https://hsbxl.be/";
+      date = "December 9th, 2024";
+    }
+    {
+      number = 7;
+      venue_name = "HSBXL";
+      venue_url = "https://hsbxl.be/";
+      date = "September 9th, 2024";
+    }
+    {
+      number = 6;
+      venue_name = "HSBXL";
+      venue_url = "https://hsbxl.be/";
+      date = "August 12th, 2024";
+    }
+    {
+      number = 5;
+      venue_name = "HSBXL";
+      venue_url = "https://hsbxl.be/";
+      date = "July 8th, 2024";
+    }
+    {
+      number = 4;
+      venue_name = "BeCentral";
+      venue_url = "https://www.becentral.org/";
+      date = "June 10th, 2024";
+    }
+    {
+      number = 3;
+      venue_name = "BeCentral";
+      venue_url = "https://www.becentral.org/";
+      date = "May 13th, 2024";
+    }
+    {
+      number = 2;
+      venue_name = "HSBXL";
+      venue_url = "https://hsbxl.be/";
+      date = "April 8th, 2024";
+    }
+    {
+      number = 1;
+      venue_name = "BeCentral";
+      venue_url = "https://www.becentral.org/";
+      date = "March 11th, 2024";
+    }
+  ];
 }
